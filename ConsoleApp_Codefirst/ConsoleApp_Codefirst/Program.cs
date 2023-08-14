8583ce8cebe28ae7b5b35bf0210bb772cd219f5d
Laroub03 <ayoublaroub@gmail.com>
2023-08-14 12:32:51 +0200

    Added SchoolMembers

--- ConsoleApp_Codefirst/ConsoleApp_Codefirst/Program.cs
+++ ConsoleApp_Codefirst/ConsoleApp_Codefirst/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+
+namespace ConsoleApp_Codefirst
+{
+    public class SchoolDbCph : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<LibraryCard> LibraryCards { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolDbCph())
+            {
+                // Perform CRUD operations here
+                // Example: Adding a student
+                var newStudent = new Student
+                {
+                    name = "John Doe",
+                    studentNumber = 12345,
+                    averageMark = 85.5
+                };
+
+                context.Students.Add(newStudent);
+                context.SaveChanges();
+
+                // Retrieve the student
+                var retrievedStudent = context.Students.FirstOrDefault(s => s.name == "John Doe");
+                Console.WriteLine($"Retrieved Student: {retrievedStudent.name}, Student Number: {retrievedStudent.studentNumber}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp_Codefirst/ConsoleApp_Codefirst/SchoolMembers.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Codefirst
+{
+
+    public class Person
+    {
+        [Required]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string phoneNumber { get; set; }
+        public string emailAdress { get; set; }
+        public Address adress { get; set; }
+        public LibraryCard libraryCard { get; set; }
+    }
+
+    public class Teacher : Person
+    {
+        public string subject { get; set; }
+        public List<Course> courses { get; set; }
+    }
+
+    public class Student : Person
+    {
+        public int studentNumber { get; set; }
+        public double averageMark { get; set; }
+        public List<Grade> grades { get; set; }
+        public List<Course> courses { get; set; }
+    }
+
+    public class LibraryCard
+    {
+        [Required]
+        public int userID { get; set; }
+        public int rentedBooks { get; set; }
+        public DateTime creationDate { get; set; }
+    }
+
+    public class Address
+    { 
+        public string street { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipcode { get; set; }
+        public string country { get; set; }
+    }
+
+    public class Course
+    {
+        [Required]
+        public int courseID { get; set; }
+        public string courseName { get; set; }
+        public List<Student> students { get; set; }
+        public List<Teacher> teachers { get; set; }
+    }
+
+    public class Grade
+    {
+        [Required]
+        public int gradeID { get; set; }
+        [Required]   
+        public string subject { get; set;}
+        [Required]
+        public int value { get; set; }
+    }
+}
